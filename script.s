c22bd6f14f9cd0f4e870ffd58b5db6be4bbd0e95
ChithaaMadanayake <chithaa.20244018@iit.ac.lk>
2025-04-26 16:10:11 +0530

    doing something

--- script.s
+++
@@ -1,8 +0,0 @@
-// 🧳 Hide splash screen after a few seconds and show content
-window.addEventListener("load", function () {
-  // Add 'loaded' class to show content after splash screen
-  setTimeout(function () {
-    document.getElementById("splash-screen").style.display = "none";
-    document.body.classList.add("loaded");
-  }, 3000); // Splash screen stays for 3 seconds
-});
---
+++ src/constants/index.js
@@ -0,0 +1,302 @@
+
+import {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  gearXpert,
+  project2,
+  project3,
+  mysql,
+  express,
+  aws,
+  mui,
+  
+  gsap,
+  framer,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  linux,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+  firstTestimonial,
+  secondTestimonial,
+  thirdTestimonial,
+} from '../assets'
+
+
+import wma from "../assets/company/wma.jpg";
+import youtube from "../assets/company/youtube.png";
+import boc from "../assets/company/boc.png";
+import peoples from "../assets/company/peoples.png";
+
+
+export const navLinks = [
+
+
+  {
+    id: "about",
+    title: "About",
+    
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Full-Stack Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Ui UX Designer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+
+  {
+    name: "Linux",
+    icon: linux,
+  },
+
+ 
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Material Ui",
+    icon: mui,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Express Js",
+    icon: express,
+  },
+
+  {
+    name: "MySql",
+    icon: mysql,
+  },
+
+  {
+    name: "git",
+    icon: git,
+  },
+ 
+
+];
+
+const experiences = [
+  {
+    title: "Web Developer   |   Digital Marketing Executive",
+    company_name: "Web Marketing Army",    
+    icon: wma,
+    iconBg: "#1B1A1A",
+    date: "Nov 2024 - present",
+    points: [
+      "Developing and maintaining company websites, ensuring high performance and SEO optimization.",
+      "Planning and executing digital marketing campaigns across various channels including social media, email, and paid ads.",
+      "Analyzing web traffic and user engagement metrics to optimize content and strategies.",
+      "Collaborating with the design and content teams to create engaging marketing materials and landing pages.",
+    ],
+  },
+  {
+    title: "Content Creator",
+    company_name: "YouTube",
+    icon: youtube, 
+    iconBg: "#FF0000",
+    date: "Mar 2021 - Present",
+    points: [
+      "Expressing creativity through visual storytelling with tutorials, process videos, and finished pieces.",
+      "Specializing in digital illustration and sharing short vlogs documenting the creative process and exploring new places.",
+      "Always seeking inspiration and building connections with the community.",
+    ],
+  },
+  {
+    title: "Internship Trainee",
+    company_name: "Bank of Ceylon",
+    icon: boc, 
+    iconBg: "#0047AB",
+    date: "Nov 2023 - May 2024",
+    points: [
+      "Gained valuable insights into banking operations during a 6-month internship.",
+      "Experience in account management, loan processing, and customer service.",
+      "Strengthened understanding of the financial industry and its role in supporting economic growth and financial well-being.",
+    ],
+  },
+  {
+    title: "Customer Care Executive",
+    company_name: "People's Bank",
+    icon: peoples, 
+    iconBg: "#006400",
+    date: "Mar 2023 - Oct 2023",
+    points: [
+      "Provided exceptional customer service by resolving inquiries, processing transactions, and offering product information.",
+      "Developed strong communication and problem-solving skills in a fast-paced environment.",
+      "Built rapport with clients, enhancing overall customer satisfaction.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Huzaif proved me wrong.",
+    name: "MD Mustaqeem",
+    designation: "Ecommerce",
+    company: "QuickMart",
+    image: firstTestimonial,
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Huzaif does.",
+    name: "Abdul Raheman",
+    designation: "Ecommerce Business",
+    company: "justbuyz",
+    image: secondTestimonial,
+  },
+  {
+    testimonial:
+      "After Huzaif optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "James Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: thirdTestimonial,
+  },
+];
+
+const projects = [
+  {
+    name: "Tekisky Mart",
+    description:
+      "Tekisky Mart is a web-based platform that allows users to search, explore, and purchase a wide range of products from various providers, offering a seamless and efficient shopping experience.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "white-text-gradient",
+      },
+      {
+        name: "node",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+ 
+    ],
+    image: project2,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "GearXpert",
+    description:
+      "GearXpert is your ultimate destination for premium auto parts and accessories, Whether you're an automobile enthusiast or a professional, find everything you need to upgrade, repair.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "emailJs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Gsap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: gearXpert,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "GoGroove-Ecommerce",
+    description:
+      "Go-Groove is your ultimate destination for premium products across various categories. Whether you're a savvy shopper or a professional, enhance, and simplify your lifestyle—all in one place!",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "express",
+        color: "white-text-gradient",
+      },
+      {
+        name: "node",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+    ],
+    image: project3,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
